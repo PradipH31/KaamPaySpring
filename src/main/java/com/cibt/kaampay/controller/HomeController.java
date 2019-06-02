c2a201047108371230bf5fdcb79a6abcb539a609
Pradip Hamal <pradip.hamal31@gmail.com>
2019-06-02 12:38:59 +0545

    Using httpServlets requests and response by invoking parent in controller

--- src/main/java/com/cibt/kaampay/controller/HomeController.java
+++ src/main/java/com/cibt/kaampay/controller/HomeController.java
@@ -5,6 +5,8 @@
  */
 package com.cibt.kaampay.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,15 +19,21 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping(value = "/")
 public class HomeController {
-    
+
     @GetMapping
-    public String index(){
+    public String index() {
         return "index";
     }
-    
+
+    @GetMapping(value = "/test")
+    public void test(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        System.out.println(request.getRemoteAddr());
+        response.getWriter().println("asdas");
+    }
+
     @GetMapping(value = "/about")
     @ResponseBody
-    public String about(){
+    public String about() {
         return "<h1>About Us</h1>";
     }
 }
